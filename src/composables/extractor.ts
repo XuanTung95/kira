bfa995880283046c8d069216fa1c427088ee4af4
tungpx <xuantung4195@gmail.com>
2025-11-27 22:45:06 +0700

    Update extractor.ts

--- src/composables/extractor.ts
+++ src/composables/extractor.ts
@@ -1,10 +1,38 @@
 
+function checkSupport(type: string | null, data: any) {
+    if (type == null) {
+        return true;
+    }
+    return true;
+}
+
+async function extract(type: string, data: any) {
+    let proxyFetch = (window as any).proxyFetch;
+    if (proxyFetch) {
+        let res: Response = await proxyFetch('https://abc.com', {
+            method: 'POST',
+            headers: {
+            },
+            body: {
+                'test': true,
+                'type': type,
+                'data': data
+            }
+        });
+        let json = await res.json();
+        console.log('json', json);
+        return json;
+    }
+    return null;
+}
+
 export function initExtractor() {
     if (window == null) {
         return;
     }
     let mWindow = window as any;
     mWindow.extractor = {
-        support: true,
+        checkSupport: checkSupport,
+        extract: extract,
     }
 }

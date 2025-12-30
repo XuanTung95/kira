ab1a0da77e5efc4e9e9012a975a5ed712966ea3d
TungPX <tungpx@meeyland.com>
2025-12-30 17:59:12 +0700

    Update hls_server.ts

--- src/composables/hls_server.ts
+++ src/composables/hls_server.ts
@@ -176,6 +176,7 @@ export function initHlsServer() {
             streamingData: data?.streamingData,
             playerConfig: data?.playerConfig,
             playabilityStatus: data?.playabilityStatus,
+            captionTracks: data?.captions?.playerCaptionsTracklistRenderer?.captionTracks,
             decodedAbrUrl: decodedAbrUrl,
             redirectHlsUrl: redirectHlsUrl,
         }
